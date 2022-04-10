c162469559b77c8f2aab2922635ce8e3518d3578
AIA <adashik585@gmail.com>
2022-04-10 12:57:01 +0600

    requier auth added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,14 +1,12 @@
 import React, { useState } from 'react';
 import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
 import './Login.css';
 
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-
-    const navigate = useNavigate();
 
     const [
         signInWithEmailAndPassword,
@@ -17,6 +15,15 @@ const Login = () => {
         error,
     ] = useSignInWithEmailAndPassword(auth);
 
+    const navigate = useNavigate();
+
+    const location = useLocation();
+    const from = location.state?.from?.pathname || '/';
+
+    if (user) {
+        navigate(from, { replace: true });
+    }
+
 
     const handleEmail = e => {
         setEmail(e.target.value);
@@ -24,10 +31,6 @@ const Login = () => {
 
     const handlePassword = e => {
         setPassword(e.target.value);
-    }
-
-    if (user) {
-        navigate('/');
     }
 
     const handleSubmit = e => {
